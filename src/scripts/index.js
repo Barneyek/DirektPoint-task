bf837000d8c43dd909605fecc4fc462b27bc9952
Damian Osiecki <osieckidamian1992@gmail.com>
2019-11-08 20:22:37 +0100

    Add burger menu with animation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,18 @@
 import '../styles/index.scss';
 
+const navSlide = () => {
+  const navBurger = document.querySelector('.nav-burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
 
+  //Toggle nav
+  navBurger.addEventListener('click', () => {
+    nav.classList.toggle('nav-links--open');
+    navLinks.forEach(link => {
+      link.classList.toggle("fade");
+    });
+  });
+
+}
+
+navSlide();
